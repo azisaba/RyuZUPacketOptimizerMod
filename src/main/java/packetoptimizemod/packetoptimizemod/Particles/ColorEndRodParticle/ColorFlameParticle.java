721869c60e11f3bfd3a4f7c7447954384d546ca3
Y_RyuZU <ryuzu1006@gmail.com>
2022-04-03 17:33:11 +0900

    リリース版

---
+++ src/main/java/packetoptimizemod/packetoptimizemod/Particles/ColorEndRodParticle/ColorFlameParticle.java
@@ -0,0 +1,72 @@
+package packetoptimizemod.packetoptimizemod.Particles.ColorEndRodParticle;
+
+import com.mojang.blaze3d.vertex.IVertexBuilder;
+import net.minecraft.client.particle.IAnimatedSprite;
+import net.minecraft.client.particle.IParticleRenderType;
+import net.minecraft.client.particle.SpriteTexturedParticle;
+import net.minecraft.client.renderer.ActiveRenderInfo;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.util.math.MathHelper;
+
+public class ColorFlameParticle extends SpriteTexturedParticle {
+    private final IAnimatedSprite spriteWithAge;
+
+    public ColorFlameParticle(
+            ClientWorld world, double x, double y, double z,
+            double motionX, double motionY, double motionZ,
+            ColorFlameParticleData particleData,
+            IAnimatedSprite spriteWithAge
+    ) {
+        super(world, x, y, z, motionX, motionY, motionZ);
+        this.spriteWithAge = spriteWithAge;
+        this.motionX *= (double) 0.1F;
+        this.motionY *= (double) 0.1F;
+        this.motionZ *= (double) 0.1F;
+        float f = (float) Math.random() * 0.4F + 0.6F;
+        this.particleRed = ((float) (Math.random() * (double) 0.2F) + 0.8F) * particleData.getRed() * f;
+        this.particleGreen = ((float) (Math.random() * (double) 0.2F) + 0.8F) * particleData.getGreen() * f;
+        this.particleBlue = ((float) (Math.random() * (double) 0.2F) + 0.8F) * particleData.getBlue() * f;
+        this.particleScale *= 0.75F * particleData.getAlpha();
+        int i = (int) (8.0D / (Math.random() * 0.8D + 0.2D));
+        this.maxAge = 4;
+        this.selectSpriteWithAge(spriteWithAge);
+    }
+
+    @Override
+    public IParticleRenderType getRenderType() {
+        return IParticleRenderType.PARTICLE_SHEET_OPAQUE;
+    }
+
+    @Override
+    public float getScale(float scaleFactor) {
+        float f = ((float)this.age + scaleFactor) / (float)this.maxAge;
+        return this.particleScale * (1.0F - f * f * 0.5F);
+    }
+
+    @Override
+    public void move(double x, double y, double z) {
+        this.setBoundingBox(this.getBoundingBox().offset(x, y, z));
+        this.resetPositionToBB();
+    }
+
+    @Override
+    public void renderParticle(IVertexBuilder buffer, ActiveRenderInfo renderInfo, float partialTicks) {
+        this.setAlphaF(0.6F - ((float)this.age + partialTicks - 1.0F) * 0.25F * 0.5F);
+        super.renderParticle(buffer, renderInfo, partialTicks);
+    }
+
+    @Override
+    public int getBrightnessForRender(float partialTick) {
+        float f = ((float)this.age + partialTick) / (float)this.maxAge;
+        f = MathHelper.clamp(f, 0.0F, 1.0F);
+        int i = super.getBrightnessForRender(partialTick);
+        int j = i & 255;
+        int k = i >> 16 & 255;
+        j = j + (int)(f * 15.0F * 16.0F);
+        if (j > 240) {
+            j = 240;
+        }
+
+        return j | k << 16;
+    }
+}
---
+++ src/main/java/packetoptimizemod/packetoptimizemod/Particles/ColorEndRodParticle/ColorFlameParticleSetup.java
@@ -0,0 +1,27 @@
+package packetoptimizemod.packetoptimizemod.Particles.ColorEndRodParticle;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.particles.ParticleType;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.ParticleFactoryRegisterEvent;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import packetoptimizemod.packetoptimizemod.PacketOptimizeMod;
+
+@Mod.EventBusSubscriber(modid = PacketOptimizeMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
+public class ColorFlameParticleSetup {
+    public static ParticleType<ColorFlameParticleData> particleType;
+
+    @SubscribeEvent
+    public static void onTypeRegistration(RegistryEvent.Register<ParticleType<?>> event) {
+        particleType = new ColorFlameParticleType();
+        particleType.setRegistryName(PacketOptimizeMod.MOD_ID, "color_flame");
+        event.getRegistry().register(particleType);
+    }
+
+    @SubscribeEvent
+    public static void onFactoryRegistration(ParticleFactoryRegisterEvent event) {
+        Minecraft.getInstance().particles.registerFactory(particleType, ColorFlameParticleFactory::new);
+    }
+}
